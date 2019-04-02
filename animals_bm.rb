caffefaceffed6a140742d71b5ee2d889c479b54
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-04-02 20:54:07 +1000

    #1 beast mode

---
+++ animals_bm.rb
@@ -0,0 +1,15 @@
+puts "List 3 groceries with a space seperating each"
+# prints user query
+
+response = Array.new
+input = gets.chomp
+# recieve user imput
+
+response << input.split(" ")
+#   we are taking splitting the string at each character that is a space
+  # \W Any non-word character
+
+  # \b Any word boundary character
+
+puts response.to_s
+# return response as a string
