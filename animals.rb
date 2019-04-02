31a4a4b5698e31a08d3964e285ac2d16cd7d68ac
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-04-02 16:48:43 +1000

    #1

---
+++ animals.rb
@@ -0,0 +1,23 @@
+four_letter_animals = ["Calf", "Duck", "Elephant", "Goat", "Lamb", "Lion", "Mule", "Dog"]
+
+
+puts four_letter_animals.push ("Puma")
+sleep 2
+
+puts four_letter_animals[4]= "Joey"
+sleep 2
+
+puts four_letter_animals.delete ("Dog")
+sleep 2
+
+puts four_letter_animals.reverse
+sleep 2
+
+puts four_letter_animals.push ("Bear")
+sleep 2
+
+puts four_letter_animals.reverse
+sleep 4
+
+
+
