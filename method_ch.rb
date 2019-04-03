f145b22ea30ffd9dc76876434831b2fa7be725d7
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-04-03 15:09:33 +1000

    #1 Commit - Methods Challenge 1

---
+++ method_ch.rb
@@ -0,0 +1,10 @@
+def say
+  puts 'Hello
+  Welcome to my application!'
+end
+
+# When typed into irb this code executes as so..
+# input: say 
+# Now will return as
+# Hello
+# Welcome to my application 
