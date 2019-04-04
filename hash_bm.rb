12ba5da6760fbdef5d9547fb0637883b3b61b323
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-04-04 15:42:02 +1000

    User input selection shows what is owed by selected customer

--- hash_bm.rb
+++ hash_bm.rb
@@ -1,5 +1,31 @@
-person = {name: 'John' , height: '2' , weight: '70kgs'}
+items = [{ customer: "John", item: "Soup", cost: 8.50}, {customer: "Sarah", item: "Pasta", cost: 12}, {customer: "John", item: "Coke", cost: 2.50}]
 
-person[:occupation] = web developer
-person [:hobbies] = art
-person [:delete] = weight
+
+array_costs_values = items.map {|x| x.values[2]}
+# john = array_costs_values.inject(0, :+) does the same as above
+# # created a variable for the values in the hash turning them into an array of intergers
+john = array_costs_values[0]
+sarah = array_costs_values[1]
+john2 = array_costs_values[2]
+# variables equate to the value of cost
+
+p "Enter a customers name ( Options: John, John2, Sarah )."
+user_getsname = gets.chomp.downcase 
+# gets user input and accounts for capitalizating
+if user_getsname == "john"
+    p "John owes $#{john}"
+end
+# dispays what is owed
+if user_getsname == "sarah"
+    p "Sarah owes $#{sarah}"
+end
+# dispays what is owed
+
+if user_getsname == "john2" 
+    p "John owes $#{john2}" 
+end  
+# dispays what is owed
+
+
+
+
