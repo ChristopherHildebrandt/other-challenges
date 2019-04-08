379baf63d4e6b681a8dca609450116c7d48307fd
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-04-08 11:47:19 +1000

    2nd

--- method_notes.rb
+++ method_notes.rb
@@ -13,4 +13,28 @@ end
 #     sentence 
 # end
 
+##########################################
+
+def greet(name)
+    greeting = "Hello #{bob}!"
+    return greeting
+end
+
+puts greet(bob) # => "hello bob!"
+
+##########################################
+puts "Whats your first name"
+first = gets.chomp
+puts "whats your last name"
+last = gets.chomp
+# gets data 
+
+def fullname (first, bob)
+    name = "#{first, last}"
+    return name
+end
+
+puts 3.times {name}
+
+
 
