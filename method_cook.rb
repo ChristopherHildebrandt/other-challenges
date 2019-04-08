0b54eb9d8756c297dc0094be5de58eb416883f90
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-04-08 12:05:09 +1000

    1st commit

---
+++ method_cook.rb
@@ -0,0 +1,12 @@
+def boil (time, item)
+puts "How to cook #{item}:"
+puts "------------------"
+puts "- Fill a saucepan with water"
+puts "- Place saucepan on the stove"
+puts "- Bring sauceplan to the boil"
+puts "- Add #{item}"
+puts "- Cook for #{time}  minutes"
+end
+
+puts boil (9, "pasta")
+puts boil (15, "rice")
