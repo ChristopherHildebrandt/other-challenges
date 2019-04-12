8f983e17ff759d2ae0e69c42eb48e8da2d457fdc
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-04-12 18:38:54 +1000

    2 commit

--- bomb_timer.rb
+++ bomb_timer.rb
@@ -1,5 +1,14 @@
+# while loop 
 bomb_timer = 10
   while bomb_timer >= 10
     puts bomb_timer
     bomb_timer -= 1
   end
+
+  # until loop
+time_fix = 100
+  until time_fix < 10
+    puts time_fix
+    time_fix -=1
+  end
+  
