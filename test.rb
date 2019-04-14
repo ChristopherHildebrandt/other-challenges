65328b6baa181c079bf2392a6fc2ad10f2815fb3
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-04-14 13:03:29 +1000

    test 5.4

--- test.rb
+++ test.rb
@@ -2,10 +2,23 @@
 # code will be deleted and added on a regular basis
 
 
-def greet(name)
-  greetings = ["Hello", "Hi", "Ohai", "ZOMG"].shuffle
-  "#{greetings.first} #{name}!"
+# def greet(name)
+#   greetings = ["Hello", "Hi", "Ohai", "ZOMG"].shuffle
+#   "#{greetings.first} #{name}!"
+# end
+
+
+# puts greet("Ada")
+
+# def miles_to_kilometers(miles)
+#   miles * 1.60934
+# end
+
+# puts miles_to_kilometers(25)
+
+def leap_year?(year) # the question mark is there so the ruby knows that its true or false 
+  year % 4 == 0 # this means if the year today when devided by 4 is the quivilant of zero  return 2
 end
 
-
-puts greet("Ada")
+p leap_year?(2012) 
+p leap_year?(2015)
