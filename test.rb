047204e3869d3dfd900e6e66a49ca4014739ad72
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-04-14 12:54:42 +1000

    test 5.2

--- test.rb
+++ test.rb
@@ -1,3 +1,11 @@
 # this is used to show commiting everyday and passion programing/coding as a career.
 # code will be deleted and added on a regular basis
 
+
+def greet(name)
+  greetings = ["Hello", "Hi", "Ohai", "ZOMG"].shuffle
+  "#{greetings.first} #{name}!"
+end
+
+
+puts greet("Ada")
