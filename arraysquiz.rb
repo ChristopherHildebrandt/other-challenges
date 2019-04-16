648953b105e83b48dc3c118a55e3a3e7a2588684
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-04-17 06:23:51 +1000

    clean up CoderAcademy-other repo

---
+++ arraysquiz.rb
@@ -0,0 +1,6 @@
+words = {"one", "two", "three", "four", "five"}
+  words.delete("two")
+  words.delete("four")
+  words
+
+p words
---
+++ class-example.rb
@@ -0,0 +1,15 @@
+# class-example.rb
+
+class Warrior
+  def initialize(name, clan, strength,dexterity)
+    @name = name
+    @race = race
+    @strength = strength
+    @dexterity = dexterity
+  end
+
+  def about 
+    puts "#{name} is a warrior from the #{clan} clan. With a strength of #{stregth},and dexterity of #{dexterity}!"
+  end
+
+  def 
