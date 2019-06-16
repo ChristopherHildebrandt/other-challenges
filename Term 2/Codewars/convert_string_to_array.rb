1588ecf4e85a891fb2a3efe06c38e1fb1a25ceff
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-06-16 14:00:37 +1000

    convert string to an array && doubling each letter of a string

---
+++ Term 2/Codewars/convert_string_to_array.rb
@@ -0,0 +1,5 @@
+def string_to_array(string)
+  string.chars
+end
+
+puts string_to_array("whats do you want from me?")
---
+++ Term 2/Codewars/loop_through_string_double_character.rb
@@ -0,0 +1,5 @@
+def double_char(string)
+  string.chars.map{|i| i*2}.join
+end
+
+puts double_char("christopher")
